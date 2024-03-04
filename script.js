6974fb50e51c623a7ea083a19e6d5ef0706f8414
rhuancavalcanti <110117224+rhuancavalcanti@users.noreply.github.com>
2024-03-04 12:28:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 imprime = (msg) => console.log(msg);
+
+fazerSorteio = () => {
+ let num1 = document.getElementById("inicio").value;
+ let num2 = document.getElementById("fim").value;
+ let sorteio = Math.floor( Math.random(incio, fim+1) * Number(num2) - Number(num1) + 1);
+
+ document.getElementById("resultado").innerHTML = sorteio;
+}
