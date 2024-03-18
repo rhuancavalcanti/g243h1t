a324efecda666f2ebd1f1c0acbb1d1194018ed5e
rhuancavalcanti <110117224+rhuancavalcanti@users.noreply.github.com>
2024-03-18 14:21:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 imprime = (msg) => console.log(msg);
-
+const data = new Date();
 function hora_atual(){
-    setTimeout(());
-document.getElementById("h").innerHTML = 
-data.getHours() + ":" data.getMinutes + ":" + getSeconds();
-},10000);
-
-hora_atual();
-
+    setTimeout(() =>{    
+   document.getElementById("h").innerHTML = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+   hora_atual();
+  }, 1000);
+}
 fazerSorteio = ()  => {
     let num1 = document.getElementById("inicio").value;
     let num2 = document.getElementById("fim").value;
@@ -19,28 +17,28 @@ fazerSorteio = ()  => {
 function escolher_aba(btn){
     for(let i = 1; i <= 4; i++){
         let id_btn = "btn"+i;
-        let id_aba = "aba"+i;
-        document.getElementById(id_btn).style.backgroundColor = "aqua";
+        let id_aba = "ab"+i;
+        document.getElementById(id_btn).style.backgroundColor = "green";
         document.getElementById(id_btn).disabled = false;
         document.getElementById(id_aba).style.display = "none";
     }
     if(btn == "1"){
-        document.getElementById("btn1").style.backgroundColor = "aquamarine";
+        document.getElementById("btn1").style.backgroundColor = "red";
         document.getElementById("btn1").disabled = true;
         document.getElementById("ab1").style.display = "block";
     }
     if(btn == "2"){
-        document.getElementById("btn2").style.backgroundColor = "aquamarine";
+        document.getElementById("btn2").style.backgroundColor = "red";
         document.getElementById("btn2").disabled = true;
         document.getElementById("ab2").style.display = "block";
     }
     if(btn == "3"){
-        document.getElementById("btn3").style.backgroundColor = "aquamarine";
+        document.getElementById("btn3").style.backgroundColor = "red";
         document.getElementById("btn3").disabled = true;
         document.getElementById("ab3").style.display = "block";
     }
     if(btn == "4"){
-        document.getElementById("btn4").style.backgroundColor = "aquamarine";
+        document.getElementById("btn4").style.backgroundColor = "red";
         document.getElementById("btn4").disabled = true;
         document.getElementById("ab4").style.display = "block";
     }
